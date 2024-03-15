c7e48a2263838c25468954fc63d6a93899004ee4
Gfecito <71032552+Gfecito@users.noreply.github.com>
2024-03-15 17:01:06 -0400

    Fixed viewer port size issues

---
+++ scripts/dynamic_camera.gd
@@ -0,0 +1,16 @@
+extends Camera2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+	# var present_zoom = get_zoom()
+	# print(present_zoom)
+	# # set_zoom( present_zoom*2 )
+	# print(get_zoom() )
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
