d05613132a7116f196755d8ecc2a5c72d1b2ae32
Gfecito <71032552+Gfecito@users.noreply.github.com>
2024-03-23 15:00:39 -0400

    Finished following behavior in enemy

--- scripts/animated_player.gd
+++ scripts/animated_player.gd
@@ -1,14 +1,28 @@
 extends AnimatedSprite2D
 
+var player
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var world_node = get_tree().root.get_child(0)
+	player = world_node.get_node("Player")
+	world_node.print_tree_pretty()
+	# TODO: Is this needed?
+	process_mode = Node.PROCESS_MODE_PAUSABLE 
 
-
+func determine_direction():
+	var player_position = player.position
+	var this_position = position
+	if player_position.x == this_position.x:
+		return 0
+	if player_position.x > this_position.x:
+		return 1
+	else:
+		return -1
+		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	var direction = Input.get_axis("move_left", "move_right")
+	var direction = determine_direction()
 	if direction != 0:
 		scale.x = abs(scale.x) * direction
 	
--- scripts/squirrel_enemy.gd
+++ scripts/squirrel_enemy.gd
@@ -1,16 +1,27 @@
 extends CharacterBody2D
 
 
-@export var movement_speed = 300.0
+@export var movement_speed = 250.0
+var player
 
 func _ready():
+	var world_node = get_tree().root.get_child(0)
+	player = world_node.get_node("Player")
+	world_node.print_tree_pretty()
 	# TODO: Is this needed?
 	process_mode = Node.PROCESS_MODE_PAUSABLE 
 
 func determine_direction():
-	return Input.get_axis("move_left", "move_right")
+	var player_position = player.position
+	var this_position = position
+	if player_position.x == this_position.x:
+		return 0
+	if player_position.x > this_position.x:
+		return 1
+	else:
+		return -1
 
-func handle_movement(delta):
+func handle_movement(_delta):
 	# Get the input direction and handle the movement/deceleration.
 	var direction = determine_direction()
 	if direction:
