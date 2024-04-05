a8e213ed208eb9b2822d3af19be66243ad642fa5
Gfecito <71032552+Gfecito@users.noreply.github.com>
2024-04-05 18:34:59 -0400

    Finished health bar

--- scripts/progress_bar.gd
+++ scripts/progress_bar.gd
@@ -1,9 +1,14 @@
 extends ProgressBar
 
+var player
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	value = 50
+	player = get_tree().root.get_child(0).get_node("Player")
+	set_min(player.MIN_SCALE.x/player.MAX_SCALE.x * 100)
+	set_max(100)
+	value = (player.scale.x / player.MAX_SCALE.x) * 100
+	print("MIN: " + str(min_value) + " MAX: " + str(max_value) + " value: " + str(value))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -11,11 +16,17 @@ func _process(delta):
 	pass
 
 
+func health():
+	return (player.scale.x / player.MAX_SCALE.x) * 100
+
+
 func _on_gui_player_damaged():
 	print("received damage signal to bar")
-	value = 10
+	set_value(health())
+	#value = 10
 
 
 func _on_gui_player_grew():
 	print("received growth signal to bar")
-	value = 100
+	set_value(health())
+	#value = 100
